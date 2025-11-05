1fb677dfb9f1e2c6f760e0417510ac8b9918cced
itskvsin <kevinsolankistudy@gmail.com>
2025-11-06 01:12:10 +0530

    removed error

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,14 @@
-<<<<<<< HEAD
 import Demo from "@/components/Demo";
 import Hero from "@/components/Hero";
 import ReactFiberPrac from "@/components/ReactFiverPrac";
-=======
-import Hero from "@/components/Hero";
->>>>>>> 68ac4ebdf4d221b9d179d09f9eabad0e31fc5194
 
 
 export default function Home() {
   return (
     <div>
-<<<<<<< HEAD
       {/* <Hero />      
       <Demo /> */}
       <ReactFiberPrac />
-=======
-      <Hero />      
->>>>>>> 68ac4ebdf4d221b9d179d09f9eabad0e31fc5194
     </div>
   );
 }
